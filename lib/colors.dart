ff3c4dc260007a62f98eb4b49eda93d820bd2648
Emran Khandaker Evan <evanemran007@gmail.com>
2023-03-15 17:28:33 +0600

    updated color codes

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 
 class AppColors {
-  static const g1 = Color(0xff43be43);
-  static const g2 = Color(0xff36d336);
-  static const g3 = Color(0xff35a235);
+  static const g1 = Color(0xff33be33);
+  static const g2 = Color(0xff2ea82e);
+  static const g3 = Color(0xff287328);
   static const g4 = Color(0xff3fa83f);
   static const g5 = Color(0xff2d982d);
 }
